00f34efff295b7aa3b32b20ba2a0968810360b07
Anggelie Lizeth Velásquez Asencio <98540747+Anggelie@users.noreply.github.com>
2024-11-21 23:44:51 -0600

    Arreglos en administración de objetos

--- app/src/main/java/com/luispedrolira/foundit/adminapp/newobject/NewObjectNavigation.kt
+++ app/src/main/java/com/luispedrolira/foundit/adminapp/newobject/NewObjectNavigation.kt
@@ -4,13 +4,17 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.luispedrolira.foundit.adminapp.presentation.newobject.NewObjectContent
+import com.luispedrolira.foundit.adminapp.presentation.dashboard.Objeto
 
-fun NavGraphBuilder.newObjectNavigation(navController: NavController) {
+fun NavGraphBuilder.newObjectNavigation(
+    navController: NavController,
+    addObject: (Objeto) -> Unit // Callback para agregar un nuevo objeto
+) {
     composable("newObject") {
         NewObjectContent(
-            onBackClick = { navController.popBackStack() },
-            navController = navController
+            onBackClick = { navController.popBackStack() }, // Regresar al stack anterior
+            navController = navController,
+            addObject = addObject // Pasar la función para agregar objetos
         )
     }
 }
-
--- app/src/main/java/com/luispedrolira/foundit/adminapp/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/luispedrolira/foundit/adminapp/presentation/dashboard/DashboardScreen.kt
@@ -1,14 +1,11 @@
 package com.luispedrolira.foundit.adminapp.presentation.dashboard
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -26,28 +23,25 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.luispedrolira.foundit.R
-import com.luispedrolira.foundit.adminapp.dashboard.Objeto
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DashboardContent1(navController: NavController) {
+fun DashboardContent1(
+    navController: NavController,
+    objeto: SnapshotStateList<Objeto>
+) {
     val objetos = remember {
         mutableStateListOf(
-            Objeto("Mochila", "Encontrado en biblioteca 10:30 am", R.drawable.mochila),
-            Objeto("Audífonos", "Encontrado en cafetería", R.drawable.audifonos),
-            Objeto("Teléfono", "Encontrado en el salón 512", R.drawable.telefono)
+            Objeto("Mochila", "Encontrado en biblioteca 10:30 am"),
+            Objeto("Audífonos", "Encontrado en cafetería"),
+            Objeto("Teléfono", "Encontrado en el salón 512")
         )
     }
 
@@ -58,9 +52,12 @@ fun DashboardContent1(navController: NavController) {
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                navController.navigate("newObject") // Navegar a NewObjectScreen
-            }) {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate("newObject") // Navegar a la pantalla para agregar objetos
+                },
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
                 Icon(Icons.Filled.Add, contentDescription = "Agregar")
             }
         }
@@ -74,7 +71,6 @@ fun DashboardContent1(navController: NavController) {
             items(objetos) { objeto ->
                 ObjetoItem(
                     objeto = objeto,
-                    onClick = { /* Acción al hacer clic en el objeto */ },
                     onDelete = { objetos.remove(objeto) }
                 )
             }
@@ -82,50 +78,43 @@ fun DashboardContent1(navController: NavController) {
     }
 }
 
-
 @Composable
-fun ObjetoItem(objeto: Objeto, onClick: () -> Unit, onDelete: () -> Unit) {
+fun ObjetoItem(
+    objeto: Objeto,
+    onDelete: () -> Unit
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .clickable { onClick() }
     ) {
         Row(
             modifier = Modifier
-                .padding(8.dp)
-                .height(80.dp),
+                .padding(16.dp)
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = objeto.imagenResourceId),
-                contentDescription = objeto.nombre,
-                modifier = Modifier
-                    .size(80.dp)
-                    .padding(end = 8.dp),
-                contentScale = ContentScale.Crop
-            )
             Column(
                 modifier = Modifier.weight(1f)
             ) {
-                Text(text = objeto.nombre, fontWeight = FontWeight.Bold)
-                Text(text = objeto.ubicacion, fontSize = 14.sp)
+                Text(
+                    text = "Nombre: ${objeto.nombre}",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = "Ubicación: ${objeto.ubicacion}",
+                    fontSize = 14.sp
+                )
             }
             Button(
                 onClick = onDelete,
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error)
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
+                modifier = Modifier
+                    .height(40.dp) // Altura consistente para el botón
             ) {
                 Text("Eliminar")
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewDashboardContent() {
-    val navController = rememberNavController()
-    MaterialTheme {
-        DashboardContent1(navController = navController)
     }
 }
